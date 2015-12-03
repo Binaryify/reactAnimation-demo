671ccd857a071e1e64727a2803412fa4e7938080
zhuangtongfa <zhuangtongfa@qq.com>
2015-12-03 20:31:24 +0800

    commit

---
+++ app/main.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactCssTransitionGroup from "react-addons-css-transition-group"
+
+var style={
+  position:"absolute",
+  right:"100px",
+  top:"20px"
+}
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      "item":[{"haha":"1212"}]
+    }
+  }
+  handleClick(){
+      var arr= this.state.item
+      arr=[...this.state.item,{"haha":"1212"}];
+      console.log(arr)
+      this.setState({
+        item:arr
+      })
+  }
+  render(){
+    var it=this.state.item.map(function(item){
+      return <div>{item.haha}</div>
+    })
+    console.log(it)
+    return(
+      <div>
+          <ReactCssTransitionGroup transitionName="example"
+          transitionEnterTimeout={500} transitionLeaveTimeout={500}>
+              {it}
+            <button onClick={ev=>this.handleClick(ev)} style={style}>add</button>
+          </ReactCssTransitionGroup>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App/>
+,document.getElementById('app'))
